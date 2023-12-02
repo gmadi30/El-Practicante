34aaf87c92ec9b35b500d5c27eeb002286b5d45a
gmadi30 <georgesmadi30@hotmail.com>
2023-12-02 15:47:47 +0100

    fix

---
+++ Backend/src/main/java/com/elpracticante/backend/degree/api/DegreeServiceAPI.java
@@ -0,0 +1,9 @@
+package com.elpracticante.backend.degree.api;
+
+import com.elpracticante.backend.degree.dto.GetDegreesResponse;
+
+public interface DegreeServiceAPI {
+
+
+    GetDegreesResponse getDegrees();
+}
